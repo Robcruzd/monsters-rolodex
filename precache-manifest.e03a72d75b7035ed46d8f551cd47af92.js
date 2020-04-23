e89bdfd10a806ed8d19d2fa1b1efd4e9e114df83
Robinson Cruz <robcruzd@hotmail.com>
2020-04-22 22:10:05 -0500

    Updates

---
+++ precache-manifest.e03a72d75b7035ed46d8f551cd47af92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896f9c5a263d6d10e6f824f88ab75ae6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3c4bac71660e763e09a1",
+    "url": "/monsters-rolodex/static/css/main.bafd7fcf.chunk.css"
+  },
+  {
+    "revision": "23e59f7830d069c92825",
+    "url": "/monsters-rolodex/static/js/2.2acd4229.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.2acd4229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4bac71660e763e09a1",
+    "url": "/monsters-rolodex/static/js/main.114ed6c7.chunk.js"
+  },
+  {
+    "revision": "696920f75d6e65de0667",
+    "url": "/monsters-rolodex/static/js/runtime-main.84355918.js"
+  }
+]);
